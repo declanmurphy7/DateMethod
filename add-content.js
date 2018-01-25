904b0f4b28ea5d3cb3dbab6e407e4272c1e1cf98
declanmurphy7 <35809332+declanmurphy7@users.noreply.github.com>
2018-01-25 17:03:51 +0000

    Version 2

--- add-content.js
+++ add-content.js
@@ -1,17 +1,34 @@
 var today = new Date();
+var day = today.getDay();
 var hourNow = today.getHours();
 var greeting;
+var weekDay;
 
-if (hourNow > 18) 
-{	greeting = 'Good Evening!';}
+if (day == 1) 
+{	weekDay = 'Monday';}
+if (day == 2) 
+{	weekDay = 'Tuesday';}
+if (day == 3) 
+{	weekDay = 'Wednesday';}
+if (day == 4) 
+{	weekDay = 'Thursday';}
+if (day == 5) 
+{	weekDay = 'Friday';}
+if (day == 6) 
+{	weekDay = 'Saturday';}
+if (day == 7) 
+{	weekDay = 'Sunday';}
 
+
+if (hourNow >= 18)
+{	greeting = 'Good evening, its' + weekDay;}
 
 else if (hourNow > 12)
-{	greeting = 'Good Afternoon!';}
+{	greeting = 'Good Afternoon Declan! Happy ' + weekDay;}
 
 
 else if (hourNow > 0) 
-{	greeting = 'Good Morning!';}
+{	greeting = 'Good Morning Declan! Happy ' + weekDay;}
 
 else
 {	greeting = 'Welcome!';}
